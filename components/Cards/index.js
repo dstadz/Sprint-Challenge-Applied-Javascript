2dc60f2b74d08ea77fbb5028adfc54ef3533166e
Drazen <danstad2012@gmail.com>
2019-10-09 19:47:49 -0700

    made dealCards function

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function dealCards() {
+
+    const cardContainer = document.querySelector('.card-container');
+
+    function cardMaker(content){
+        //create main card div
+        let newcard = document.createElement('div');
+        newcard.classList.add('card');
+
+        //create headline & author
+        let headline = document.createElement('div');
+        let author = document.createElement('div');
+        let imgContainer = document.createElement('div');
+        let img = document.createElement('img');
+        let span = document.createElement('span');
+
+        //add relevant classes
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imgContainer.classList.add('img-container')
+
+        //nesting classes
+        newcard.appendChild(headline)
+        newcard.appendChild(author)
+        author.appendChild(imgContainer)
+        author.appendChild(span)
+        imgContainer.appendChild(img)
+
+        return newcard
+    }
+
+
+
+
+        newcard.textContent = content;
+        
+        return newcard;
+    }
+
+}
+dealCards()
